ae6b7f302513e881d508c2852dd388b64d91dee2
prateek959 <prateekkush8081@gmail.com>
2026-02-11 12:01:06 +0530

    Add database

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 
 const db = async(req, res)=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL_DEV);
+        await mongoose.connect(process.env.MONGODB_URL_PROD);
         console.log('DB connected successfully');
     } catch (error) {
         console.log(error);
